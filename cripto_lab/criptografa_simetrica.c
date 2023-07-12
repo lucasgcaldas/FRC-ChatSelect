0625fd7c065e3dc4fe8696b31fbd1ed64d839854
João Gabriel de Matos <joaogabriel.c.matos@gmail.com>
2023-07-12 17:44:44 -0300

    Adiciona codigo que verifica se o arquivo foi realmente encriptado.

--- cripto_lab/criptografa_simetrica.c
+++ cripto_lab/criptografa_simetrica.c
@@ -6,10 +6,10 @@
 #define BUFFER_SIZE 4096
 #define HEADER_SIZE 54
 
-void encryptFile(const char* inputFile, const char* outputFile, const char* key)
+void encryptFile(const char *inputFile, const char *encryptedFile, const char *key)
 {
     FILE *fin = fopen(inputFile, "rb");
-    FILE *fout = fopen(outputFile, "wb");
+    FILE *fout = fopen(encryptedFile, "wb");
 
     unsigned char inputBuffer[BUFFER_SIZE];
     unsigned char outputBuffer[BUFFER_SIZE];
@@ -43,10 +43,10 @@ void encryptFile(const char* inputFile, const char* outputFile, const char* key)
     printf("Chave de desencriptação: %s\n", key);
 }
 
-void decryptFile(const char *inputFile, const char *outputFile, const char *key)
+void decryptFile(const char *inputFile, const char *encryptedFile, const char *key)
 {
     FILE *fin = fopen(inputFile, "rb");
-    FILE *fout = fopen(outputFile, "wb");
+    FILE *fout = fopen(encryptedFile, "wb");
 
     unsigned char inputBuffer[BUFFER_SIZE];
     unsigned char outputBuffer[BUFFER_SIZE];
@@ -79,17 +79,73 @@ void decryptFile(const char *inputFile, const char *outputFile, const char *key)
     printf("Desencriptação concluída com sucesso.\n");
 }
 
+int compareFiles(const char *file1, const char *file2)
+{
+    FILE *fp1 = fopen(file1, "rb");
+    FILE *fp2 = fopen(file2, "rb");
+
+    if (fp1 == NULL || fp2 == NULL)
+    {
+        perror("Erro ao abrir os arquivos");
+        return -1;
+    }
+
+    fseek(fp1, 0, SEEK_END);
+    fseek(fp2, 0, SEEK_END);
+
+    long fileSize1 = ftell(fp1);
+    long fileSize2 = ftell(fp2);
+
+    if (fileSize1 != fileSize2)
+    {
+        fclose(fp1);
+        fclose(fp2);
+        return 0; // Arquivos têm tamanhos diferentes
+    }
+
+    fseek(fp1, 0, SEEK_SET);
+    fseek(fp2, 0, SEEK_SET);
+
+    int byte1, byte2;
+    int differentBytes = 0;
+
+    while ((byte1 = fgetc(fp1)) != EOF && (byte2 = fgetc(fp2)) != EOF)
+    {
+        if (byte1 != byte2)
+        {
+            differentBytes = 1;
+            break;
+        }
+    }
+
+    fclose(fp1);
+    fclose(fp2);
+
+    return !differentBytes;
+}
+
 int main()
 {
     const char *inputFile = "fractaljulia.bmp";
-    const char *outputFile = "arquivo_encriptado.bmp";
-    const char *outputFile2 = "arquivo_desencriptado.bmp";
+    const char *encryptedFile = "arquivo_encriptado.bmp";
+    const char *decryptedFile = "arquivo_desencriptado.bmp";
 
     const char *key = "chave123";
 
-    encryptFile(inputFile, outputFile, key);
+    encryptFile(inputFile, encryptedFile, key);
 
-    decryptFile(outputFile, outputFile2, key);
+    decryptFile(encryptedFile, decryptedFile, key);
+
+    int isEncrypted = compareFiles(inputFile, encryptedFile);
+
+    if (isEncrypted)
+    {
+        printf("O arquivo foi encriptado corretamente.\n");
+    }
+    else
+    {
+        printf("O arquivo não foi encriptado corretamente.\n");
+    }
 
     return 0;
 }
